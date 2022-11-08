23a2c1de387d1c3798f17435a1a60a1e61be6d5a
Jarod Weigand <jarodw@pandpdigitalhealth.com>
2022-11-07 20:01:43 -0500

    Commit changes to BouncyBall

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var dx = 2
+var dy = 0.9
+var x = 0
+var y = 0
+
+function setup() {
+   x = 0
+   y = 0
+  createCanvas(400, 400);
+}
+
+function draw() {
+  x = x + dx
+  y = y + dy
+  if((x > 400) | (x<0)){
+    dx = -dx
+  }
+  if((y > 400) | (y < 0)){
+    dy = -dy
+  }
+  background(220);
+  circle(x,y,10)
+}
